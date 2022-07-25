aac26cd6120fdf3002902bb9fe337b185f180040
MariIvChin <mari.chin1976@gmail.com>
2022-07-25 10:18:03 +0300

    finally

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const inputName = document.getElementById("input-name");
 const inputAge = document.getElementById("input-age");
 const inputColor = document.getElementById("input-color");
 const inputCountry = document.getElementById("input-country");
-
 const inputMale = document.getElementById("input-male");
 const inputFemale = document.getElementById("input-female");
-
 const userCard = document.getElementById("user-card");
 
 function handleSubmitButton() {
   if (inputName.value.length <= "2") {
     alert('Name must to be more than 2 char in the field "name"!');
     return;
-  } else if (inputAge.value <= "0") {
+  }
+  if (inputAge.value <= "0") {
     alert('Please set number more than 0 in the field "age"!');
     return;
-  } else if (inputCountry.value == "") {
+  }
+  if (inputCountry.value == "") {
     alert("We must to know where you live! Please choose your country!");
     return;
-  } else if (getUserGenderToUserCard() == undefined) {
+  }
+  if (getUserGenderToUserCard() == undefined) {
     alert("Please set your gender for complete your info for us!");
     return;
   }
